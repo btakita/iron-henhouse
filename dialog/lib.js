7ba9a215e8bb7d36a427277df9313da442042e75
Colin Cahill <colincahill@gmail.com>
2016-06-07 12:53:10 -0700

    bad ideas come in threes

--- dialog/lib.js
+++ dialog/lib.js
@@ -1,35 +1,47 @@
-import {assign} from "ctx-core/object/lib";
-import {log,debug} from "ctx-core/logger/lib";
+import {
+    assign
+}
+from "ctx-core/object/lib";
+import {
+    log, debug
+}
+from "ctx-core/logger/lib";
 const logPrefix = "dialog/lib";
-export function assign__route$dialog$map(ctx) {
-  log(`${logPrefix}|fn$route$dialog$map`);
-  if (ctx.route$dialog$map) return ctx;
-  assign(ctx, {
-    route$dialog$map: {
-      "spt-addtocalendar-dialog": {
-        tag$name: "spt-addtocalendar-dialog",
-        "title": "Add to Calendar"
-      },
-      "spt-check-registration-dialog": {
-        tag$name: "spt-check-registration-dialog",
-        title: "Check Your Voter Registration"
-      },
-      "spt-election-day-menu-dialog": {
-        tag$name: "spt-election-day-menu-dialog"
-      },
-      "spt-early-voting-dialog": {
-        tag$name: "spt-early-voting-dialog",
-        title: "Early Voting"
-      },
-      "spt-early-voting-issue-dialog": {
-        tag$name: "spt-early-voting-issue-dialog",
-        title: "Get Help!"
-      },
-      "spt-zip-code-dialog": {
-        tag$name: "spt-zip-code-dialog",
-        title: "Where do you vote?"
-      }
-    }
-  });
-  return ctx;
+export
+
+function assign__route$dialog$map(ctx) {
+    log(`${logPrefix}|fn$route$dialog$map`);
+    if (ctx.route$dialog$map) return ctx;
+    assign(ctx, {
+        route$dialog$map: {
+            "spt-addtocalendar-dialog": {
+                tag$name: "spt-addtocalendar-dialog",
+                "title": "Add to Calendar"
+            },
+            "spt-check-registration-dialog": {
+                tag$name: "spt-check-registration-dialog",
+                title: "Check Your Voter Registration"
+            },
+            "spt-election-day-menu-dialog": {
+                tag$name: "spt-election-day-menu-dialog"
+            },
+            "spt-early-voting-dialog": {
+                tag$name: "spt-early-voting-dialog",
+                title: "Early Voting"
+            },
+            "spt-early-voting-issue-dialog": {
+                tag$name: "spt-early-voting-issue-dialog",
+                title: "Get Help!"
+            },
+            "spt-zip-code-dialog": {
+                tag$name: "spt-zip-code-dialog",
+                title: "Where do you vote?"
+            },
+            "spt-i-voted-dialog": {
+                tag$name: "spt-i-voted-dialog",
+                title: "Please tell us about your voting experience:"
+            }
+        }
+    });
+    return ctx;
 }
--- election-day/route.js
+++ election-day/route.js
@@ -1,78 +1,126 @@
-import {assign,clone} from "ctx-core/object/lib";
-import {route,fn$route as route$lib__fn$route,assign__route$$} from "ctx-core/route/lib";
-import {ctx$reset} from "site/lib";
-import {assign__voting_issue$$} from "voting-issue/lib";
-import {log,debug} from "ctx-core/logger/lib";
+import {
+    assign, clone
+}
+from "ctx-core/object/lib";
+import {
+    route, fn$route as route$lib__fn$route, assign__route$$
+}
+from "ctx-core/route/lib";
+import {
+    ctx$reset
+}
+from "site/lib";
+import {
+    assign__voting_issue$$
+}
+from "voting-issue/lib";
+import {
+    log, debug
+}
+from "ctx-core/logger/lib";
 const logPrefix = "election-day/route";
-export function assign__election_day$route$$() {
-  log(`${logPrefix}|assign__election_day$route$$`);
-  let ctx = assign(...arguments);
-  assign__voting_issue$$(ctx);
-  return assign__route$$(
-    ctx,
-    fn$route(ctx, {path: "", route$name: "election_day_root"}),
-    fn$route(ctx, {path: "\\?*", route$name: "election_day_root"}),
-    fn$route(ctx, {path: "reset", route$name: "reset", fn: reset__route}),
-    fn$route(ctx, {path: "vote-where", route$name: "vote_where"}),
-    fn$route(ctx, {path: "vote-where\\?*", route$name: "vote_where"}),
-    fn$route(ctx, {path: "voting-issue", route$name: "voting_issue"}),
-    fn$route(ctx, {path: "voting-issue\\?*", route$name: "voting_issue"}),
-    ...fn$voting_issue$route$$(ctx)
-  );
+export
+function assign__election_day$route$$() {
+    log(`${logPrefix}|assign__election_day$route$$`);
+    let ctx = assign(...arguments);
+    assign__voting_issue$$(ctx);
+    return assign__route$$(
+        ctx,
+        fn$route(ctx, {
+            path: "",
+            route$name: "election_day_root"
+        }),
+        fn$route(ctx, {
+            path: "\\?*",
+            route$name: "election_day_root"
+        }),
+        fn$route(ctx, {
+            path: "reset",
+            route$name: "reset",
+            fn: reset__route
+        }),
+        fn$route(ctx, {
+            path: "vote-where",
+            route$name: "vote_where"
+        }),
+        fn$route(ctx, {
+            path: "vote-where\\?*",
+            route$name: "vote_where"
+        }),
+        fn$route(ctx, {
+            path: "voting-issue",
+            route$name: "voting_issue"
+        }),
+        fn$route(ctx, {
+            path: "voting-issue\\?*",
+            route$name: "voting_issue"
+        }),
+        ...fn$voting_issue$route$$(ctx)
+    );
 }
+
 function fn$voting_issue$route$$(ctx, ...ctx$rest$$) {
-  log(`${logPrefix}|fn$voting_issue$route$$`);
-  let ctx$rest = clone(...ctx$rest$$)
-    , route$$;
-  route$$ = ctx.voting_issue$$.reduce(
-    (memo, voting_issue) => {
-      const voting_issue$key = `voting_issue$${voting_issue}`
-          , voting_issue$route$path$ = voting_issue$route$path(voting_issue)
-          , voting_issue$path = `voting-issue/${voting_issue$route$path$}`;
-      memo.push(
-        fn$route(ctx, {
-          path: voting_issue$path,
-          route$name: voting_issue$key,
-          fn: root__route}, ctx$rest),
-        fn$route(ctx, {
-          path: `${voting_issue$path}\\?*`,
-          route$name: voting_issue$key,
-          fn: root__route}, ctx$rest)
-      );
-      return memo;
-      function root__route(route$ctx) {
-        assign(route$ctx, {
-          voting_issue$key: voting_issue$key,
-          voting_issue$tile: true,
-          voting_issue$route$path: voting_issue$route$path$
-        });
-      }
-    }, []);
-  return route$$;
+    log(`${logPrefix}|fn$voting_issue$route$$`);
+    let ctx$rest = clone(...ctx$rest$$),
+        route$$;
+    route$$ = ctx.voting_issue$$.reduce(
+        (memo, voting_issue) => {
+            const voting_issue$key = `voting_issue$${voting_issue}`,
+                voting_issue$route$path$ = voting_issue$route$path(voting_issue),
+                voting_issue$path = `voting-issue/${voting_issue$route$path$}`;
+            memo.push(
+                fn$route(ctx, {
+                    path: voting_issue$path,
+                    route$name: voting_issue$key,
+                    fn: root__route
+                }, ctx$rest),
+                fn$route(ctx, {
+                    path: `${voting_issue$path}\\?*`,
+                    route$name: voting_issue$key,
+                    fn: root__route
+                }, ctx$rest)
+            );
+            return memo;
+
+            function root__route(route$ctx) {
+                assign(route$ctx, {
+                    voting_issue$key: voting_issue$key,
+                    voting_issue$tile: true,
+                    voting_issue$route$path: voting_issue$route$path$
+                });
+            }
+        }, []);
+    return route$$;
 }
+
 function voting_issue$route$path(voting_issue) {
-  return voting_issue.replace(/_/g, "-");
+    return voting_issue.replace(/_/g, "-");
 }
+
 function reset__route(route$ctx) {
-  log(`${logPrefix}|reset__route`);
-  ctx$reset(route$ctx);
-  route(route$ctx, "/");
+    log(`${logPrefix}|reset__route`);
+    ctx$reset(route$ctx);
+    route(route$ctx, "/");
 }
+
 function fn$route(ctx, ...ctx$rest$$) {
-  return route$lib__fn$route(ctx, {fn$route$ctx: fn$ctx}, ...ctx$rest$$);
+    return route$lib__fn$route(ctx, {
+        fn$route$ctx: fn$ctx
+    }, ...ctx$rest$$);
 }
+
 function fn$ctx() {
-  log(`${logPrefix}|fn$ctx`);
-  return assign({
-    route$name__reset: null,
-    route$name__election_day_root: null,
-    route$name__vote_where: null,
-    route$name__voting_issue: null,
-    voting_issue$title: null,
-    voting_issue$tile: null,
-    voting_issue$route$path: null
-  }, ...ctx.voting_issue$$.reduce((memo, voting_issue) => {
-      memo[`route$name__voting_issue$${voting_issue}`] = null;
-      return memo;
+    log(`${logPrefix}|fn$ctx`);
+    return assign({
+        route$name__reset: null,
+        route$name__election_day_root: null,
+        route$name__vote_where: null,
+        route$name__voting_issue: null,
+        voting_issue$title: null,
+        voting_issue$tile: null,
+        voting_issue$route$path: null
+    }, ...ctx.voting_issue$$.reduce((memo, voting_issue) => {
+        memo[`route$name__voting_issue$${voting_issue}`] = null;
+        return memo;
     }, {}), ...arguments);
 }
